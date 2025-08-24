ac0f24515234a29f6e105305a1ccc44f6a3762d4
agevavava <holhokgorgok@gmail.com>
2025-08-24 13:38:51 +0800

    Add files via upload
    Optimasi sedikit

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-# angka: int = int(input("Masukan angka:"))
-
-def angkajaditeks(angka):
+def num2word(angka):
     satuan: list = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan']
-    
+
     angka = int(angka)
     if angka == 0:
         return 0
     
-    def satudigit(i):
-        print(f"{satuan[i]}")
-        return 0
-    
-    def duadigit(i):
+    def belas_puluh(i):
         digit1 = int(i / 10)
         digit2 = int(i % 10)
         if digit1 == 1 and digit2 == 1:
             print("sebelas")
             return 0
+        elif digit1 == 1:
+            print(f"{satuan[digit2]} belas")
+            return 0
+        elif digit1 == 0:
+            print(f"{satuan[digit2]}")
         else:
             print(f"{satuan[digit1]} puluh {satuan[digit2]}")
             return 0
 
-    def tigadigit(i):
+    def ratus(i):
         digit1 = int(i / 100)
         digit2 = int((i - (digit1 * 100)) / 10)
         digit3 = int(i - (digit1 * 100) - (digit2 * 10))
@@ -40,19 +39,17 @@ def angkajaditeks(angka):
         else:
             print(f"{satuan[digit1]} ratus {satuan[digit2]} puluh {satuan[digit3]}")
 
-    if angka < 10:
-        satudigit(angka)
-    elif len(str(angka)) == 2:
-        duadigit(angka)
-    elif len(str(angka)) == 3:
-        tigadigit(angka)
+    if angka < 100:
+        belas_puluh(angka)
+    elif angka < 1000:
+        ratus(angka)
     else:
         print("Error!: Maksimal input adalah 999")
 
 def main():
     try:
         nomer = input("Masukin angka: ")
-        angkajaditeks(nomer)
+        num2word(nomer)
     except:
         ValueError
         print("Error! Harus masukan angka")
